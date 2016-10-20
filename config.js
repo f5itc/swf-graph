0fef1ae406d2346126b9f5499b29ab4b4bd22f63
Daryl Collins <D.Collins@f5.com>
2016-10-20 00:59:04 -0700

    Update controller calls in bin to receive config as an object rather than expecting a file path, and update sample config file to export object instead of function

--- config.js
+++ config.js
@@ -1,40 +1,38 @@
-'use strict';
+var path = require('path');
 
-let path = require('path');
+var config = {
+  defaultVersion: '1.0.0',
 
-module.exports = function getConfig() {
-  return {
-    defaultVersion: '1.0.0',
+  region: process.env.AWS_REGION || 'us-west-1',
 
-    region: process.env.AWS_REGION || 'us-west-1',
+  activities: [
+    path.join(__dirname, 'activities')
+  ],
 
-    activities: [
-      path.join(__dirname, 'activities')
-    ],
+  workflows: [
+    path.join(__dirname, 'workflows')
+  ],
 
-    workflows: [
-      path.join(__dirname, 'workflows')
-    ],
-
-    notifier:   {
-      region:       process.env.AWS_REGION || 'us-west-1',
-      snsTopicName: 'ftl-engine-alerts',
-      awsAccountId: '548238529753'
-    },
-    logger:     {
-      name: 'ftl-engine'
-    },
-    swf:        {
-      domainName:   process.env.SWF_DOMAIN || 'DCF5',
-      workflowName: 'TestWorkflow'
-    },
-    metrics:    {
-      host: 'localhost',
-      port: 8125
-    },
-    claimCheck: {
-      bucket: 'ftl-check-store',
-      prefix: ''
-    }
+  notifier:   {
+    region:       process.env.AWS_REGION || 'us-west-1',
+    snsTopicName: 'ftl-engine-alerts',
+    awsAccountId: '548238529753'
+  },
+  logger:     {
+    name: 'ftl-engine'
+  },
+  swf:        {
+    domainName:   process.env.SWF_DOMAIN || 'DCF5',
+    workflowName: 'TestWorkflow'
+  },
+  metrics:    {
+    host: 'localhost',
+    port: 8125
+  },
+  claimCheck: {
+    bucket: 'ftl-check-store',
+    prefix: ''
   }
 };
+
+module.exports = config;
